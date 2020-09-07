70dc1f1c6f6da87260807076f74741ce8c45fd6f
Nath Sarkar, Disary <disary.nath.sarkar@accenture.com>
2020-09-07 12:26:45 +0530

    Initialize all the test class

---
+++ Payment.BRules.Test/MembershipTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Payment.BRules.Test
+{
+    [TestClass]
+    public class MembershipTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
