c57f1abe7a23b180dbb19d97b4eddf726e2375c3
Nath Sarkar, Disary <disary.nath.sarkar@accenture.com>
2020-09-07 11:54:00 +0530

    added method to read input xml

--- Payment.BRules/Program.cs
+++ Payment.BRules/Program.cs
@@ -3,13 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
 
 namespace Payment.BRules
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
+        }
+
+        public static string ReadInputRequest()
+        {
+            string paymentType = "";
+            XmlDocument model = new XmlDocument();
+            string directory = @"Input\PaymentInfo.xml";
+            model.Load(directory);
+            foreach (XmlNode node in model.DocumentElement.ChildNodes)
+            {
+                if (node.Name == "PaymentType")
+                {
+                    paymentType = node.InnerText;
+                }
+            }
+            return paymentType;
         }
     }
 }
