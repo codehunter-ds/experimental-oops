353127b9fdd66cdaa5e8bff1762de715b7d54e53
Nath Sarkar, Disary <disary.nath.sarkar@accenture.com>
2020-09-07 12:31:19 +0530

    UT for physical product payment test

--- Payment.BRules.Test/PhysicalProductTest.cs
+++ Payment.BRules.Test/PhysicalProductTest.cs
@@ -1,14 +1,33 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using RulesEngine.Rules;
 
 namespace Payment.BRules.Test
 {
     [TestClass]
     public class PhysicalProductTest
     {
+     
         [TestMethod]
-        public void TestMethod1()
+        public void ValidateRules()
         {
+            var membership = new PhysicalProduct();
+            var result = membership.Handle("physical");
+
+            // Assert
+            Assert.AreEqual("Create a packing slip for shipping", result); 
+
+        }
+
+        [TestMethod]
+        public void CheckNullRules()
+        {
+            var membershipMock = new Mock<PhysicalProduct> { CallBase = true };
+            var result = membershipMock.Setup(m => m.Handle("new")).Returns("Activate Membership");
+            // Assert
+            Assert.IsNotNull(result);   
+
         }
     }
 }
